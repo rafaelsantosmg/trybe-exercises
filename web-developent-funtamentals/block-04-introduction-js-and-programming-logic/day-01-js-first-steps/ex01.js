6f88a39f751fba6c24e6db7904327521accd068c
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-08-26 13:02:31 -0300

    Exercicios de 1 ao 3

---
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex01.js
@@ -0,0 +1,20 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+let a = 10;
+let b = 7;
+
+console.log(a + b, "\n");
+
+console.log(a - b, "\n");
+
+console.log(a * b, "\n");
+
+console.log(a / b, "\n");
+
+console.log(a % b, "\n");
---
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex02.js
@@ -0,0 +1,10 @@
+/* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. */
+
+const NUM1 = 17;
+const NUM2 = 32;
+
+if (NUM1 > NUM2)
+  console.log("O maior número é: 'NUM1'", NUM1);
+else {
+  console.log("O maior número é: 'NUM2'", NUM2);
+}
