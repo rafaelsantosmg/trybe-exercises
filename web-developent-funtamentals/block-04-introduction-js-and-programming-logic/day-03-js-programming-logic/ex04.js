4ba683b492eea731633b00e0128bca8e45ba2641
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-08-27 19:58:31 -0300

    Exercicios de 4.3 ex04 piramide

---
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-03-js-programming-logic/ex04.js
@@ -0,0 +1,14 @@
+/* Depois, faça uma pirâmide com n asteriscos de base: */
+
+let base = 5
+let string = ""
+
+if (base % 2 !== 0) {
+  for (let l = 0; l < base; l += 2) {
+    for (let c = 0; c < base; c += 2) {
+      if (c )
+      string += "*"
+      console.log(string)
+    }
+  }
+}
