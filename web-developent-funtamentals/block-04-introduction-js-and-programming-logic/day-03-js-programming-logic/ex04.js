6ec1021ba5acd7ce50581fc448c2d3d2988bd96d
Rafael Santos <rafaelalonsobarbosa@gmail.com>
2021-08-28 20:13:57 -0300

    EX 04

--- web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-03-js-programming-logic/ex04.js
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-03-js-programming-logic/ex04.js
@@ -1,14 +1,21 @@
 /* Depois, faça uma pirâmide com n asteriscos de base: */
 
-let base = 5
+let base = 9
 let string = ""
+let lString = (base + 1) / 2
+let rString = (base + 1) / 2
+let inicio = (base + 1) / 2
 
-if (base % 2 !== 0) {
-  for (let l = 0; l < base; l += 2) {
-    for (let c = 0; c < base; c += 2) {
-      if (c )
+for (let l = 0; l < base; l += 2) {
+  for (let c = 1; c <= base; c += 1) {
+    if (c < lString || c > rString) {
+      string += " "
+    } else {
       string += "*"
-      console.log(string)
     }
   }
+  lString -= 1
+  rString += 1
+  console.log(string)
+  string = ""
 }
