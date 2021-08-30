b916c3875c9ce62592d084f8671b2e2cf36dcd01
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-08-30 19:24:20 -0300

    Refatoraçao ex06

--- web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex06.js
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex06.js
@@ -5,27 +5,33 @@
 * Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 Exemplo: bishop (bispo) -> diagonals (diagonais) */
 
-const chessPiece = "ReI";
+const CHESSPIECE = "Rei";
 
-switch (chessPiece.toLowerCase()) {
-  case 'rei':
-    console.log("O 'REI' pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.");
-    break;
-  case "rainha":
-    console.log(`A "RAINHA" move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.`);
-    break;
-  case "bispo":
-    console.log(`O "BISPO" move-se ao longo da diagonal. Não pode pular outras peças.`);
-    break;
-  case "cavalo":
-    console.log(`É a única peça que pode pular as outras. O movimento do "CAVALO" é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.`);
-    break;
-  case "torre":
-    console.log(`A "TORRE" movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.`);
-    break;
-  case "peao":
-    console.log(`O "PEÃO" movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.`);
-    break;
-  default:
-    console.log(`ERRO! Peça ${chessPiece} Inexistente!!!`);
+function chessPiece(chessPiece) {
+  let choice;
+  switch (chessPiece.toLowerCase()) {
+    case 'rei':
+      choice = `O 'REI' pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.`;
+      break;
+    case "rainha":
+      choice = `A "RAINHA" move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.`;
+      break;
+    case "bispo":
+      choice = `O "BISPO" move-se ao longo da diagonal. Não pode pular outras peças.`;
+      break;
+    case "cavalo":
+      choice = `É a única peça que pode pular as outras. O movimento do "CAVALO" é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.`;
+      break;
+    case "torre":
+      choice = `A "TORRE" movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.`;
+      break;
+    case "peao":
+      choice = `O "PEÃO" movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.`;
+      break;
+    default:
+      choice = `ERRO! Peça ${chessPiece} Inexistente!!!`;
+  }
+  return console.log(choice);
 }
+
+chessPiece(CHESSPIECE);
