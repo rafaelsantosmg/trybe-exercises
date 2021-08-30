cfa9b0b24be6299e65b697494a46007ca3a3e2da
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-08-29 22:24:12 -0300

    Exercicio 4 concluido

--- web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-03-js-programming-logic/ex04.js
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-03-js-programming-logic/ex04.js
@@ -1,12 +1,12 @@
 /* Depois, faça uma pirâmide com n asteriscos de base: */
 
-let base = 9
+let base = 5
 let string = ""
-let lString = (base + 1) / 2
-let rString = (base + 1) / 2
-let inicio = (base + 1) / 2
 
-for (let l = 0; l < base; l += 2) {
+let lString = Math.round(base / 2)
+let rString = Math.round(base / 2)
+
+for (l = 0; l < base; l += 2) {
   for (let c = 1; c <= base; c += 1) {
     if (c < lString || c > rString) {
       string += " "
