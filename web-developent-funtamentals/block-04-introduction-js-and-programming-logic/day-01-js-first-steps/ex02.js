50558f5cf0c10b082c988199af18f39ef58c9be7
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-08-30 18:33:21 -0300

    Refatoraçao ex02

--- web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex02.js
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex02.js
@@ -1,10 +1,14 @@
 /* Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. */
 
-const NUM1 = 17;
+const NUM1 = 87;
 const NUM2 = 32;
 
-if (NUM1 > NUM2)
-  console.log("O maior número é: 'NUM1'", NUM1);
-else {
-  console.log("O maior número é: 'NUM2'", NUM2);
+function biggeSmaller(num1, num2) {
+  if (num1 > num2) {
+    return console.log(`O maior número é: ${num1}`);
+  } else {
+    return console.log(`O maior número é: ${num2}`);
+  }
 }
+
+biggeSmaller(NUM1, NUM2);
