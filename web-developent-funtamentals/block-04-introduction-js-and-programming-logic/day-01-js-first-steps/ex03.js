093a7a5bb270c4b1e2f222281c95ec66fda3812f
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-08-30 18:38:33 -0300

    Refatoraçao ex03

--- web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex03.js
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex03.js
@@ -1,13 +1,20 @@
 /* Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. */
 
 const NUM1 = 42;
-const NUM2 = 40;
+const NUM2 = 50;
 const NUM3 = 41;
 
-if (NUM1 > NUM2 && NUM1 > NUM3) {
-  console.log("O maior número é: 'NUM1'", NUM1)
-} else if (NUM2 > NUM3) {
-  console.log("O maior número é: 'NUM2'", NUM2)
-} else {
-  console.log("O maior número é: 'NUM3'", NUM3)
+function biggeSmaller(num1, num2, num3) {
+  let result;
+
+  if (num1 > num2 && num1 > num3) {
+    result = num1
+  } else if (num2 > num3) {
+    result = num2
+  } else {
+    result = num3
+  }
+  return console.log(`O maior número é: ${result}`)
 }
+
+biggeSmaller(NUM1, NUM2, NUM3);
