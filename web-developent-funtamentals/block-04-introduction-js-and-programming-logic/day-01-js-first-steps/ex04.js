09ab76422ace92dde1678a02ffd73ca63161f548
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-08-30 18:45:34 -0300

    Refatoraçao ex04

--- web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex04.js
+++ web-developent-funtamentals/block-04-introduction-js-and-programming-logic/day-01-js-first-steps/ex04.js
@@ -1,11 +1,18 @@
 /* Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. */
 
-const number = 0;
+const NUMBER = 0;
 
-if (number > 0) {
-  console.log("Positivo");
-} else if (number < 0) {
-  console.log("Negativo");
-} else {
-  console.log("Zero");
+function positiveNegative(number) {
+  let result;
+
+  if (number > 0) {
+    result = "Positivo";
+  } else if (number < 0) {
+    result = "Negativo";
+  } else {
+    result = "Zero";
+  }
+  return console.log(result);
 }
+
+positiveNegative(NUMBER);
