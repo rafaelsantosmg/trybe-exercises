7d19e9f79957cdaf9a31c1aa0fc1dc8336ca89fd
rafaelsantos <rafaelalonsobarbosa@gmail.com>
2021-09-03 19:31:23 -0300

    Brincando com Selectores

---
+++ web-developent-funtamentals/block-05-js-dom-events-web-storage/day-01-js-dom-selectors/script.js
@@ -0,0 +1,10 @@
+let colorCyan = ['#00FFFF', '#0000FF', '#778899', '#A0522D', '#FFA500'];
+
+document.getElementById('container').style.backgroundColor = colorCyan[2];
+
+document.getElementById('header-container').style.color = colorCyan[4];
+
+let classEmergencyTasks = document.getElementsByClassName('emergency-tasks');
+
+let classNoEmergencyTasks = document.getElementsByClassName('no-emergency-tasks');
+
