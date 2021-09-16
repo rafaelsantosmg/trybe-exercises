bb6bfbfafca448473174abe3a82db5ee6d742ece
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-15 23:22:53 -0300

    implementaçao da funçao do botao submit

--- web-developent-funtamentals/block-06-html-css-forms-flexbox-responsive/day-01-html-css-forms/script.js
+++ web-developent-funtamentals/block-06-html-css-forms-flexbox-responsive/day-01-html-css-forms/script.js
@@ -1,4 +1,18 @@
+const selectForm = document.querySelector('#formulario');
+const selectMain = document.querySelector('#main');
 const selectStats = document.querySelector('#estados');
+const selectInputName = document.querySelector('#name');
+const selectInputEmail = document.querySelector('#email');
+const selectInputCpf = document.querySelector('#cpf');
+const selectInputEnd = document.querySelector('#endereco');
+const selectInputCid = document.querySelector('#cidade');
+const selectInputTipoCs = document.querySelector('#casa');
+const selectInputTipoApt = document.querySelector('#apartamento');
+const selectInputResumo = document.querySelector('#resumo');
+const selectInputCargo = document.querySelector('#cargo');
+const selectInputDescCargo = document.querySelector("#desc-cargo");
+const selectInputData = document.querySelector('#data-ini');
+const selectButtonSubmit = document.querySelector('#enviar');
 const states = {
   'AC': 'Acre',
   'AL': 'Alagoas',
@@ -29,10 +43,33 @@ const states = {
   'TO': 'Tocantins'
 }
 
+window.onload = ()=> {
+  for (let key in states) {
+    const createOption = document.createElement('option');
+    createOption.value = key;
+    createOption.innerHTML = states[key];
+    selectStats.appendChild(createOption);
+  }
+};
 
-for (let key in states) {
-  const createOption = document.createElement('option');
-  createOption.value = key;
-  createOption.innerHTML = states[key];
-  selectStats.appendChild(createOption);
-}
+selectButtonSubmit.addEventListener('click', (event)=> {
+  event.preventDefault();
+  const createDivPersonal = document.createElement('div')
+  createDivPersonal.classList.add('printuser');
+  createDivPersonal.innerHTML = `<h2>Dados Pessoais</h2>`;
+  createDivPersonal.innerHTML += `<p>Nome: ${selectInputName.value}</p>`;
+  createDivPersonal.innerHTML += `<p>Email: ${selectInputEmail.value}</p>`;
+  createDivPersonal.innerHTML += `<p>CPF: ${selectInputCpf.value}</p>`;
+  createDivPersonal.innerHTML += `<p>Endereço: ${selectInputEnd.value}</p>`;
+  createDivPersonal.innerHTML += `<p>Cidade: ${selectInputCid.value}</p>`;
+  createDivPersonal.innerHTML += `<p>Estado: ${selectStats.value}</p>`;
+  selectMain.appendChild(createDivPersonal);
+  const createDivFirstJob = document.createElement('div')
+  createDivFirstJob.classList.add('printuser');
+  createDivFirstJob.innerHTML = `<h2>Primeiro Emprego</h2>`;
+  createDivFirstJob.innerHTML += `<p>Resumo: ${selectInputResumo.value}</p>`;
+  createDivFirstJob.innerHTML += `<p>Cargo: ${selectInputCargo.value}</p>`;
+  createDivFirstJob.innerHTML += `<p>Descrição do Cargo: ${selectInputDescCargo.value}</p>`;
+  createDivFirstJob.innerHTML += `<p>Data de Inicio: ${selectInputData.value}</p>`;
+  selectMain.appendChild(createDivFirstJob);
+});
