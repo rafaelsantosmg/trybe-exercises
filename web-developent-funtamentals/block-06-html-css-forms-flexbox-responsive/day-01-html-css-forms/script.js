d9a334487cd1a86900675909d48ed1a75c9a8865
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-15 21:13:08 -0300

    Form dados pessoais

---
+++ web-developent-funtamentals/block-06-html-css-forms-flexbox-responsive/day-01-html-css-forms/script.js
@@ -0,0 +1,38 @@
+const selectStats = document.querySelector('#estados');
+const states = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+}
+
+
+for (let key in states) {
+  const createOption = document.createElement('option');
+  createOption.value = key;
+  createOption.innerHTML = states[key];
+  selectStats.appendChild(createOption);
+}
