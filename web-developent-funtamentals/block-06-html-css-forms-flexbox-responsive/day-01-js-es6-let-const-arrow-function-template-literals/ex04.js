f8461194925e0e66ec844edbb6d5346d967f9765
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-23 19:26:12 -0300

    exercicio 4 refatorado

--- web-developent-funtamentals/block-06-html-css-forms-flexbox-responsive/day-01-js-es6-let-const-arrow-function-template-literals/ex04.js
+++ web-developent-funtamentals/block-06-html-css-forms-flexbox-responsive/day-01-js-es6-let-const-arrow-function-template-literals/ex04.js
@@ -1,10 +1,14 @@
-const longestWord = strings => {
-  const totCaracter = strings.split(' ');
-  let string = totCaracter[0];
-  for (let index = 1; index < totCaracter.length; index += 1) {
-    string.length > totCaracter[index].length ? string = string : string = totCaracter[index];
-  }
-  console.log(string);
-}
+// const longestWord = strings => {
+//   const totCaracter = strings.split(' ');
+//   let string = totCaracter[0];
+//   for (let index = 1; index < totCaracter.length; index += 1) {
+//     string.length > totCaracter[index].length ? string = string : string = totCaracter[index];
+//   }
+//   console.log(string);
+// }
 
-longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+// longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
