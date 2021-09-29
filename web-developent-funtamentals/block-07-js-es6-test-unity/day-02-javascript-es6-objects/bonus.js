3742ed63d154982eb7113cabf7eeb9e8192b39ee
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-29 16:43:12 -0300

    teste

--- web-developent-funtamentals/block-07-js-es6-test-unity/day-02-javascript-es6-objects/bonus.js
+++ web-developent-funtamentals/block-07-js-es6-test-unity/day-02-javascript-es6-objects/bonus.js
@@ -1,5 +1,4 @@
-/** Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5.
-Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5: */
+/** Crie uma função para contar quantos estudantes assistiram às aulas de Matemática. Use o objeto criado no exercício 5. */
 
 const lesson1 = {
   materia: 'Matemática',
@@ -21,15 +20,19 @@ const lesson3 = {
   turno: 'noite',
 };
 
-const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
-console.log(allLessons);
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 
-const getNumberOfStudents = (obj) => {
-  let total = 0;
-  const array = Object.keys(obj);
-  for (index in array) {
-    total += obj[array[index]].numeroEstudantes;
+const countStudent = (obj, math) => {
+  let tot = 0;
+  for (key in obj) {
+    if (obj[key].materia === math) {
+      tot += obj[key].numeroEstudantes;
+    }
   }
-  return total;
-};
-console.log(getNumberOfStudents(allLessons));
+  return tot;
+} 
+
+console.log(countStudent(allLessons, 'Matemática'))
+/** Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5: */
+
+
