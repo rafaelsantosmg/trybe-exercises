d4044dfaa8b0b6cde92743f324eb3f7e49ded66e
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-29 13:23:44 -0300

    pokedex

--- front-end/block-10-introduction-to-reack/day-02-components-react/my_app/src/App.js
+++ front-end/block-10-introduction-to-reack/day-02-components-react/my_app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Pokedex from './components/Pokedex';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Pokedex />
+      </>
+    );
+  }
 }
 
 export default App;
