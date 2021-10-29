ab339b34509770eb9e0ec16fc795463a63e54056
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-10-29 00:31:01 -0300

    realização exercício 10.2

--- front-end/block-10-introduction-to-reack/day-02-components-react/my_app/src/App.js
+++ front-end/block-10-introduction-to-reack/day-02-components-react/my_app/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import './App.css';
+import Pokedex from './components/Pokedex';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        
-      </div>
+      <>
+        <Header />
+        <Pokedex />
+      </>
     );
   }
 }
