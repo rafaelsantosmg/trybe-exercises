4e062db6f9be55e0d11db1e177746a0f282ea096
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-01-03 14:57:05 -0300

    Iniciando estudo REDUX

---
+++ front-end/block-15-state-management-redux/day-01-redux-state-global-aplication/index.js
@@ -0,0 +1,10 @@
+const Redux = require('redux');
+const store = Redux.createStore();
+
+const reducer = (state) => {
+  return state;
+};
+
+const store = createStore(reducer);
+
+console.log(store);
